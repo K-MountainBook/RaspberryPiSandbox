fbe7cbf7bb707f5aec1beae1e8a29a62880c32a3
Keita Yamamoto <keita.yamamoto.1009@gmail.com>
2023-02-27 10:12:27 +0900

    202302271012

--- nfclab/classes/Database.py
+++ nfclab/classes/Database.py
@@ -9,8 +9,12 @@ class DataBase():
 
         ホスト名の設定が存在しない場合defaultの設定を読み込む。
         """
+        logs = OutputLogs.OutputLogs()
+
         configfile = open("config.json", "r")
         jsonData = json.load(configfile)
+
+        logs.output(self.__class__.__name__, "environment:" + env)
 
         if env not in jsonData:
             env = "default"
@@ -26,7 +30,7 @@ class DataBase():
         標準出力に出力される。
         """
         log = OutputLogs.OutputLogs()
-        log.output(self.__class__.__name__, str(self.con.is_connected()))
+        log.output(self.__class__.__name__, "db is connected:" + str(self.con.is_connected()))
 
         # print('[{}] [{}] db is connected:{}'.format(datetime.datetime.now(),
         #       str(self.con.is_connected())))
